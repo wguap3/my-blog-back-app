cd4c05fed25c7a09d6d4ac9ce065ad7ed1578bd2
3wguap <rnbrell@ya.ru>
2026-02-25 16:22:57 +0400

    перписал тесты

--- src/test/java/blog/MyBlogBackAppApplicationTests.java
+++ src/test/java/blog/MyBlogBackAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyBlogBackAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
